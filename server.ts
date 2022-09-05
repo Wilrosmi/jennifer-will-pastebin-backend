c4ea045b11a4b19279ce3cb92d85f2de52c3ee41
Wilrosmi <will.r.smith31@gmail.com>
2022-09-05 17:07:57 +0100

    Basic working server

--- server.ts
+++ server.ts
@@ -3,6 +3,12 @@ import { config } from "dotenv";
 import express from "express";
 import cors from "cors";
 
+
+const app = express();
+
+app.use(express.json()); //add body parser to each following route handler
+app.use(cors()) //add CORS support to each following route handler
+
 config(); //Read .env file lines as though they were env vars.
 
 //Call this script with the environment variable LOCAL set if you want to connect to a local db (i.e. without SSL)
@@ -15,19 +21,21 @@ const herokuSSLSetting = { rejectUnauthorized: false }
 const sslSetting = process.env.LOCAL ? false : herokuSSLSetting
 const dbConfig = {
   connectionString: process.env.DATABASE_URL,
-  ssl: sslSetting,
-};
+  ssl: {
+    rejectUnauthorized: false
+  }
+}
 
-const app = express();
-
-app.use(express.json()); //add body parser to each following route handler
-app.use(cors()) //add CORS support to each following route handler
 
 const client = new Client(dbConfig);
-client.connect();
+
+async function connecToDb(): Promise<void> {
+  await client.connect();
+}
+connecToDb();
 
 app.get("/", async (req, res) => {
-  const dbres = await client.query('select * from posts order by post_date desc');
+  const dbres = await client.query('select * from posts order by post_date desc limit 10');
   res.status(200).json({data: dbres.rows});
 });
 
@@ -40,13 +48,13 @@ app.get<{id: string}>("/:id", async (req, res) => {
   }
 });
 
-app.post<{}, {}, {post: string}>("/", async (req, res) => {
-  const {post} = req.body;
-  if (typeof post === 'string') {
-    const dbres = await client.query('insert into posts (post) values ($1)', [post]);
-    res.status(200).json({status: "succes"});
-  } {
-    res.status(400).json({status: "wrong post type"});
+app.post<{}, {}, {message: string, title?: string}>("/", async (req, res) => {
+  const {message, title} = req.body; 
+  if (typeof message === 'string' && message.length > 0 && (typeof title === 'string' || typeof title === 'undefined')) {
+    const dbres = await client.query('insert into posts (message, title) values ($1, $2)', [message, title]);
+    res.status(200).json({status: "success"});
+  } else {
+    res.status(400).json({status: "wrong input type"});
   }
 });
 
